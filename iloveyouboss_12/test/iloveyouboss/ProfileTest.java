d80488d624b26eb47ba16941ba9cbebe070ebf3a
GwangHun Moon <mkh9293@gmail.com>
2019-08-26 00:26:54 +0900

    Add 4 Chapter
    2019-08-26
    테스트 코드는 동작에 따라 분리해야함

---
+++ iloveyouboss_12/test/iloveyouboss/ProfileTest.java
@@ -0,0 +1,33 @@
+package iloveyouboss;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * 이런 식으로 하나의 테스트 메소드의 여러 동작을 검증하는 것은 좋지 못함.
+ * 테스트 분리 필요.
+ */
+public class ProfileTest {
+
+    @Test
+    public void matches() {
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        Question question = new BooleanQuestion(1, "Got milk?");
+
+        // must-match 항목이 맞지 않으면 false
+        profile.add(new Answer(question, Bool.FALSE));
+        Criteria criteria = new Criteria();
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
+
+        assertFalse(profile.matches(criteria));
+
+        // don't care 항목에 대해서는 true
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria = new Criteria();
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
+
+        assertTrue(profile.matches(criteria));
+    }
+}
